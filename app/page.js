28d3186a37efb96066c7d4bf7063574433ac0057
Jacey Davis <jaceyd923@gmail.com>
2024-02-02 16:39:06 -0700

    loading animations for home screen

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
 'use client'
-import { useContext } from "react"
+import { useState, useContext, useEffect } from "react"
 import { ProgressDataContext } from "./context/ProgressDataContext"
 
 export default function Home() {
   const { progressData, setProgressData } = useContext(ProgressDataContext)
-  const completedTags = Object.keys(progressData).filter(tag => progressData[tag].completed)
-  const uncompletedTags = Object.keys(progressData).filter(tag => !progressData[tag].completed)
+  const [completedTags, setCompletedTags] = useState(null)
+  const [uncompletedTags, setUncompletedTags] = useState(null)
+
+  useEffect(() => {
+    if (progressData) {
+      setCompletedTags(Object.keys(progressData).filter(tag => progressData[tag].completed))
+      setUncompletedTags(Object.keys(progressData).filter(tag => !progressData[tag].completed))
+    }
+  }, [progressData])
 
 
   return (
@@ -15,41 +22,55 @@ export default function Home() {
       <h2 className="text-2xl font-bold">To Find</h2>
 
       <div className="mt-3 flex flex-col gap-4">
-        {uncompletedTags.length ? (
-          uncompletedTags.map((tag, i) => (
-            <div key={i} className="flex items-center gap-3 bg-accent p-3 rounded-lg font-medium">
-              <figure className="w-1/5"><img src="/images/placeholder-icon.png" alt=""/></figure>
-              <p>{progressData[tag].description}</p>
-              <button className="btn btn-secondary btn-sm rounded-full">Hint</button>
+        {uncompletedTags ? (
+          uncompletedTags.length > 0 ? (
+            uncompletedTags.map((tag, i) => (
+              <div key={i} className="flex items-center gap-3 bg-accent p-3 rounded-lg font-medium">
+                <figure className="w-1/5"><img src="/images/placeholder-icon.png" alt="" /></figure>
+                <p>{progressData[tag].description}</p>
+                <button className="btn btn-secondary btn-sm rounded-full">Hint</button>
+              </div>
+            ))
+          ) : (
+            <div className="bg-neutral text-center p-3 rounded-lg font-medium">
+              <p className="font-bold mb-2">You have found and completed all the tags!</p>
+              <a href="/huntcompleted" className="link">See certificate →</a>
             </div>
-          ))
+          )
         ) : (
-          <div className="bg-neutral text-center p-3 rounded-lg font-medium">
-            <p className="font-bold mb-2">You have found and completed all the tags!</p>
-            <a href="/huntcompleted" className="link">See certificate →</a>
+          <div className='text-center'>
+            <span className="loading loading-spinner loading-lg text-primary"></span>
           </div>
         )}
-        
+
+
       </div>
 
       {/* COMPLETED TAGS */}
       <h2 className="text-2xl font-bold my-5">Completed</h2>
 
       <div className="mt-3 flex flex-col gap-4">
-        {completedTags.length ? (
-          completedTags.map((tag, i) => (
-            <div key={i} className="flex items-center gap-3 bg-[#7d8376] p-3 rounded-lg font-medium">
-            <figure className="w-1/5"><img src="/images/placeholder-icon.png" alt=""/></figure>
-            <p>{progressData[tag].description}</p>
-            <p className="text-3xl">✓</p>
-          </div>
-          ))
+        {completedTags ? (
+          completedTags.length > 0 ? (
+            completedTags.map((tag, i) => (
+              <div key={i} className="flex items-center gap-3 bg-[#7d8376] p-3 rounded-lg font-medium">
+                <figure className="w-1/5"><img src="/images/placeholder-icon.png" alt="" /></figure>
+                <p>{progressData[tag].description}</p>
+                <p className="text-3xl">✓</p>
+              </div>
+            ))
+          ) : (
+            <div className="bg-neutral text-center p-3 rounded-lg font-medium">
+              <p className="font-bold">0 Completed</p>
+              <p>Search the field station for the white tags to get started</p>
+            </div>
+          )
         ) : (
-          <div className="bg-neutral text-center p-3 rounded-lg font-medium">
-            <p className="font-bold">0 Completed</p>
-            <p>Search the field station for the white tags to get started</p>
+          <div className='text-center'>
+            <span className="loading loading-spinner loading-lg text-primary"></span>
           </div>
         )}
+
       </div>
     </main>
   )
