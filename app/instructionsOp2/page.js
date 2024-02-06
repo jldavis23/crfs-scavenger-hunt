63e879f56a56eb00df830c2de46ed59f6bbee1b8
Jacey Davis <jaceyd923@gmail.com>
2024-02-05 18:07:12 -0700

    instructions page options

---
+++ app/instructionsOp2/page.js
@@ -0,0 +1,67 @@
+'use client'
+import { useState } from 'react'
+
+export default function InstructionsPage({ setProgressData }) {
+    const instructions = [
+        {
+            text: 'Search inside the field station for small white tags/chips/discs attached to the walls.',
+            imagePath: '/images/NFC-chip.jpg',
+            alt: 'An example of a tag/chip/disc'
+        },
+        {
+            text: 'When you have located a tag, place your phone over it to bring up a new webpage.',
+            imagePath: '/images/scan-animation.gif',
+            alt: 'Animation demonstrating how to scan the chip'
+        },
+        {
+            text: 'Read the information on the page and complete the activity.',
+            imagePath: '',
+            alt: ''
+        },
+        {
+            text: 'Look for more tags! Your progress will be tracked in a progress bar at the top of the screen.',
+            imagePath: '',
+            alt: ''
+        },
+        {
+            text: 'Once you have located and completed the activity for each tag, show your completed screen to the site manager to earn a small prize!',
+            imagePath: '',
+            alt: ''
+        },
+        {
+            text: 'Important! Make sure to NOT use a private/incognito tab and to NOT clear your browsing history while playing the scavenger hunt or else your progress will not be saved correctly!',
+            imagePath: '',
+            alt: ''
+        }
+    ]
+
+    return (
+        <div className="bg-neutral p-5 flex flex-col gap-10">
+            <div className='flex gap-5 justify-between'>
+                <figure><img src='/images/crfs-logo.png' alt='CRFS Logo' className="w-20" /></figure>
+                <figure><img src='/images/uvu-dgm-logo.png' alt="UVU DGM Logo" className="w-32" /></figure>
+            </div>
+
+            <div className="flex flex-col gap-4">
+                <h1 className="text-4xl font-semibold font-cabinSketch">Welcome to the Captiol Reef Field Station Scavenger Hunt!</h1>
+
+                <div className='flex flex-col gap-12 mb-10'>
+                    {instructions.map((step, i) => (
+                        <div key={i}>
+                            <div className="flex gap-3 mb-3">
+                                <p className="text-4xl font-semibold text-accent flex items-center">{i + 1}</p>
+                                <p className="text-lg">{step.text}</p>
+                            </div>
+
+                            <img src={step.imagePath} alt={step.alt} />
+                        </div>
+                    ))}
+                </div>
+
+                <button className="btn btn-primary border border-white text-white">START</button>
+                <p className="text-center">© UVU DWDD 490R | 2024</p>
+            </div>
+
+        </div>
+    )
+}
