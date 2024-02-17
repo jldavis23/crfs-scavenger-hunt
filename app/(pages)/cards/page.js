b03f9d52e829336498a41f99c9290cf5e99ea519
Jacey Davis <jaceyd923@gmail.com>
2024-02-16 21:58:47 -0700

    progress on matching game

--- app/(pages)/cards/page.js
+++ app/(pages)/cards/page.js
@@ -16,74 +16,74 @@ export default function CardsPage() {
         },
         {
             id: 2,
-            isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            isImage: false,
+            text: 'Cassidy Arch',
             match: 1
         },
         {
             id: 3,
             isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            imagePath: '/images/cardGameImages/chimney-rock-card.jpg',
             match: 4
         },
         {
             id: 4,
-            isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            isImage: false,
+            text: 'Chimney Rock',
             match: 3
         },
         {
             id: 5,
             isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            imagePath: '/images/cardGameImages/fruita-card.jpg',
             match: 6
         },
         {
             id: 6,
-            isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            isImage: false,
+            text: 'Fruita',
             match: 5
         },
         {
             id: 7,
             isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            imagePath: '/images/cardGameImages/golden-throne-card.jpg',
             match: 8
         },
         {
             id: 8,
-            isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            isImage: false,
+            text: 'Golden Throne',
             match: 7
         },
         {
             id: 9,
             isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            imagePath: '/images/cardGameImages/hickman-bridge-card.jpg',
             match: 10
         },
         {
             id: 10,
-            isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            isImage: false,
+            text: 'Hickman Bridge',
             match: 9
         },
         {
             id: 11,
             isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            imagePath: '/images/cardGameImages/crfs-card.jpg',
             match: 12
         },
         {
             id: 12,
-            isImage: true,
-            imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
+            isImage: false,
+            text: 'Capitol Reef Field Station',
             match: 11
         },
     ]
     
     return (
-        <main>
+        <main className="p-5">
             <MatchingCards 
                 cards={cards}
             />
--- app/components/MatchingCards.js
+++ app/components/MatchingCards.js
@@ -4,11 +4,36 @@ import styles from './MatchingCards.module.css';
 import confetti from 'canvas-confetti'
 
 export const MatchingCards = ({ cards, matchingCompleted, setMatchingCompleted }) => {
-    const [isFlipped, setIsFlipped] = useState(false)
+    const [selectedCards, setSelectedCards] = useState([])
+    const [allCards, setAllCards] = useState(cards.map(card => ({ ...card, isMatched: false })))
+
+    useEffect(() => {
+        if (selectedCards.length === 2) {
+            if (selectedCards[0].match === selectedCards[1].id) {
+                const newCards = allCards.map(card => card.id === selectedCards[0].id || card.id === selectedCards[1].id ? { ...card, isMatched: true } : card)
+
+                setTimeout(() => {
+                    setAllCards(newCards)
+                    setSelectedCards([])
+                }, 1000)
+            } else {
+                console.log('cards DO NOT MATCH AAAA')
+                setTimeout(() => {
+                    setSelectedCards([])
+                }, 1000)
+            }
+        }
+    }, [selectedCards])
+
+    const handleCardFlip = (cardId, cardMatch) => {
+        if (selectedCards.length < 2) {
+            setSelectedCards([...selectedCards, { id: cardId, match: cardMatch }])
+        }
+    }
 
     return (
         <section className='grid grid-rows-4 grid-cols-3 gap-3'>
-            {cards.map(card => (
+            {/* {cards.map(card => (
                 <div key={card.id} className={styles.scene}>
                     <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} onClick={() => setIsFlipped(!isFlipped)}>
                         <figure className={`${styles.cardFace} ${styles.front}`}>
@@ -20,7 +45,20 @@ export const MatchingCards = ({ cards, matchingCompleted, setMatchingCompleted }
                         </div>
                     </div>
                 </div>
+            ))} */}
+
+            {allCards.map(card => (
+                <div key={card.id} className='hover:cursor-pointer rounded-lg border-2 border-info ' onClick={() => handleCardFlip(card.id, card.match)}>
+                    {selectedCards.some(c => c.id === card.id) ? (
+                        card.isImage ? <img src={card.imagePath} alt='' className='rounded-lg' /> : <p>{card.text}</p>
+                    ) : (
+                        <img src='/images/cardGameImages/card-front.png' alt='' className='rounded-lg' />
+                    )}
+
+                </div>
+
             ))}
         </section>
+
     )
 }
