bbc59574b53949e8e3b991b1827cc8426d8bc36b
Jacey Davis <jaceyd923@gmail.com>
2024-02-17 22:51:47 -0700

    app recognizes matching cards

--- app/(pages)/cards/page.js
+++ app/(pages)/cards/page.js
@@ -6,86 +6,110 @@ import { MatchingCards } from '@/app/components/MatchingCards';
 import { HuntCompleteModal } from '../../components/HuntCompleteModal';
 
 export default function CardsPage() {
-
-    let cards = [
+    const [cards, setCards] = useState([
         {
             id: 1,
             isImage: true,
             imagePath: '/images/cardGameImages/cassidy-arch-card.jpg',
-            match: 2
+            match: 2,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 2,
             isImage: false,
             text: 'Cassidy Arch',
-            match: 1
+            match: 1,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 3,
             isImage: true,
             imagePath: '/images/cardGameImages/chimney-rock-card.jpg',
-            match: 4
+            match: 4,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 4,
             isImage: false,
             text: 'Chimney Rock',
-            match: 3
+            match: 3,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 5,
             isImage: true,
             imagePath: '/images/cardGameImages/fruita-card.jpg',
-            match: 6
+            match: 6,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 6,
             isImage: false,
             text: 'Fruita',
-            match: 5
+            match: 5,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 7,
             isImage: true,
             imagePath: '/images/cardGameImages/golden-throne-card.jpg',
-            match: 8
+            match: 8,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 8,
             isImage: false,
             text: 'Golden Throne',
-            match: 7
+            match: 7,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 9,
             isImage: true,
             imagePath: '/images/cardGameImages/hickman-bridge-card.jpg',
-            match: 10
+            match: 10,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 10,
             isImage: false,
             text: 'Hickman Bridge',
-            match: 9
+            match: 9,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 11,
             isImage: true,
             imagePath: '/images/cardGameImages/crfs-card.jpg',
-            match: 12
+            match: 12,
+            isFlipped: false,
+            isMatched: false
         },
         {
             id: 12,
             isImage: false,
             text: 'Capitol Reef Field Station',
-            match: 11
+            match: 11,
+            isFlipped: false,
+            isMatched: false
         },
-    ]
+    ])
     
     return (
         <main className="p-5">
             <MatchingCards 
                 cards={cards}
+                setCards={setCards}
             />
         </main>
     )
--- app/components/MatchingCards.js
+++ app/components/MatchingCards.js
@@ -3,60 +3,51 @@ import { useState, useEffect } from 'react';
 import styles from './MatchingCards.module.css';
 import confetti from 'canvas-confetti'
 
-export const MatchingCards = ({ cards, matchingCompleted, setMatchingCompleted }) => {
+export const MatchingCards = ({ cards, setCards, matchingCompleted, setMatchingCompleted }) => {
     const [selectedCards, setSelectedCards] = useState([])
-    const [allCards, setAllCards] = useState(cards.map(card => ({ ...card, isMatched: false })))
 
     useEffect(() => {
-        if (selectedCards.length === 2) {
-            if (selectedCards[0].match === selectedCards[1].id) {
-                const newCards = allCards.map(card => card.id === selectedCards[0].id || card.id === selectedCards[1].id ? { ...card, isMatched: true } : card)
+        setSelectedCards(cards.filter(card => card.isFlipped))
+    }, [cards])
 
-                setTimeout(() => {
-                    setAllCards(newCards)
-                    setSelectedCards([])
-                }, 1000)
+    useEffect(() => {
+        if (selectedCards.length > 1) {
+            if (selectedCards[0].match === selectedCards[1].id) {
+                console.log('MATCH!')
             } else {
-                console.log('cards DO NOT MATCH AAAA')
-                setTimeout(() => {
-                    setSelectedCards([])
-                }, 1000)
+                console.log('no match...')
             }
         }
     }, [selectedCards])
 
-    const handleCardFlip = (cardId, cardMatch) => {
-        if (selectedCards.length < 2) {
-            setSelectedCards([...selectedCards, { id: cardId, match: cardMatch }])
+    const handleCardFlip = (card) => {
+        if (card.isFlipped) {
+            const updatedCards = cards.map(c => c.id === card.id ? {...c, isFlipped: !c.isFlipped} : c)
+            setCards(updatedCards)
+        } else {
+            if (selectedCards.length < 2) {
+                const updatedCards = cards.map(c => c.id === card.id ? {...c, isFlipped: !c.isFlipped} : c)
+                setCards(updatedCards)
+            } 
         }
+        
     }
 
     return (
-        <section className='grid grid-rows-4 grid-cols-3 gap-3'>
-            {/* {cards.map(card => (
-                <div key={card.id} className={styles.scene}>
-                    <div className={`${styles.card} ${isFlipped ? styles.flipped : ''}`} onClick={() => setIsFlipped(!isFlipped)}>
+        <section className='flex flex-wrap justify-center gap-3'>
+            {cards.map(card => (
+                <div key={card.id} className={`${styles.scene}`}>
+                    <div className={`${styles.card} ${card.isFlipped ? styles.flipped : ''}`} onClick={() => handleCardFlip(card)}>
                         <figure className={`${styles.cardFace} ${styles.front}`}>
-                            <img src="/images/cardGameImages/card-front.png" />
+                            {/* <img src="/images/cardGameImages/card-front.png" className='rounded-lg' /> */}
                         </figure>
 
                         <div className={`${styles.cardFace} ${styles.back}`}>
-                            <img src={card.imagePath} alt="" />
+                            {card.isImage ? <img src={card.imagePath} alt="" className='rounded' /> : <p className='h-full p-1 text-center flex justify-center items-center'>{card.text}</p>}
+                            
                         </div>
                     </div>
                 </div>
-            ))} */}
-
-            {allCards.map(card => (
-                <div key={card.id} className='hover:cursor-pointer rounded-lg border-2 border-info ' onClick={() => handleCardFlip(card.id, card.match)}>
-                    {selectedCards.some(c => c.id === card.id) ? (
-                        card.isImage ? <img src={card.imagePath} alt='' className='rounded-lg' /> : <p>{card.text}</p>
-                    ) : (
-                        <img src='/images/cardGameImages/card-front.png' alt='' className='rounded-lg' />
-                    )}
-
-                </div>
-
             ))}
         </section>
 
